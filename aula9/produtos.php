039b093c25c983185dc936db1a47cf82f404f8de
dudaaoliveira <maria6.souza@alunos.ifsuldeminas.edu.br>
2023-03-22 09:21:43 -0300

    aula10

--- aula9/produtos.php
+++ aula9/produtos.php
@@ -2,20 +2,25 @@
    $produto1 = [
                "nome" => "Câmera",
                "marca" => "Lg",
-               "preço" => 2500 ];
+               "preço" => 2500
+            ];
 
    $produto2 = [
                 "nome" => "Smatphone",
                 "marca" => "Sansung",
-                "preço" => 3500 ];
+                "preço" => 3500 
+               ];
 
    $produto3 = [
                     "nome" => "Notebook",
                     "marca" => "Dell",
-                    "preço" => 7500 ];
+                    "preço" => 7500 
+                  ];
 
+   //matriz
    $produtos = [$produto1, 
                 $produto2, 
-                $produto3];
+                $produto3
+               ];
 
    echo json_encode($produtos);
