2371c6d4a37d6b67b7c6554f4496adb80f413cb8
dudaaoliveira <maria6.souza@alunos.ifsuldeminas.edu.br>
2023-03-29 10:37:50 -0300

    aula12

--- aula11/exemplo_post/login.php
+++ aula11/exemplo_post/login.php
@@ -1,7 +1,12 @@
 <?php 
 
-if($POST['usuario'] -- "adimin" && $_POST['senha'] == 123){
+if(isset($POST['usuario'])){
+    if ($_POST['usuario'] == "adimin" && $_POST['senha'] ===123){
     echo "Usuário logado";
-}else{
+}
+else{
     echo "Usuário ou senha inválidos"
+}
+}else{
+    header("location: form.html");
 }
